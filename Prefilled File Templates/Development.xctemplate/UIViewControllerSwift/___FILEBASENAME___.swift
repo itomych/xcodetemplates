13929d219fbc38997278b9de09ffae989e808c4b
Kirill Ushkov <kirillushkov@benders-MacBook-Pro-5.local>
2019-01-23 17:22:46 +0200

    PROD-2882 added view controller template

---
+++ Prefilled File Templates/Development.xctemplate/UIViewControllerSwift/___FILEBASENAME___.swift
@@ -0,0 +1,19 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created on ___DATE___.
+//___COPYRIGHT___
+//
+
+import UIKit
+
+class ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_cocoaTouchSubclass___ {
+    private struct Constants {
+        <#here insert your constants#>
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
